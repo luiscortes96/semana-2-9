1214a641d55e481d907c6d56f546bd85ea94599e
luiscortes96 <luiscortes1996@hotmail.com>
2020-12-04 18:22:29 -0500

    falta desarrollas las imagenes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
+    <h1 class="h1 text-center">{{titulo}}</h1>
+    <Noticias></Noticias>
   </div>
 </template>
 
 <script>
+import Noticias from './components/Noticias.vue'
 
 export default {
   name: 'App',
+  components: {
+    Noticias
+  },
+  data(){
+    return{
+      titulo : 'Noticias Colombia'
+    }
+  }
 }
 </script>
 
---
+++ src/components/Noticias.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="row px-5">
+        <div v-for="item in informacion" :key="item.id" class=".col-6 .col-md-4 mx-auto my-4"> 
+           <div class="card mb-3 h-100" style="max-width: 550px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                         <!-- <img src="{{ item.urlToImage }}" class="card-img" alt="..."> -->
+                    </div>
+                    <div class="col-md-8">
+                         <div class="card-body">
+                            <h5 class="card-title">{{ item.title }}</h5>
+                            <p class="card-text">{{ item.description }}</p>
+                            <p class="card-text"><small class="text-muted">{{ item.publishedAt }}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>  
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Noticias',
+    props: {
+
+    }, 
+    data(){
+        return{
+            informacion:[] ,
+
+        }
+  },
+  
+  mounted (){
+      axios
+      .get('http://newsapi.org/v2/top-headlines?country=co&apiKey=d7f504a8743c44e19015be237c251dcc')
+      .then(respuesta =>this.informacion=respuesta.data.articles)
+      
+  }  
+}
+
+
+
+</script>
+
+<style scoped>
+
+</style>
