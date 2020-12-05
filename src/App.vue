3e7981e1c44750e5bdf5dbcec4f1b677d37832a8
luiscortes96 <luiscortes1996@hotmail.com>
2020-12-05 15:24:49 -0500

    cambios

--- src/App.vue
+++ src/App.vue
@@ -14,19 +14,48 @@
 <script>
 import Noticias from './components/Noticias.vue'
 import FooterPage from './components/FooterPage.vue'
-
+import TeamCard from './components/TeamCard.vue'
 
 
 export default {
   name: 'App',
   components: {
     Noticias,
-    FooterPage
+    FooterPage,
+    TeamCard
   },
 
   data(){
     return{
-      titulo :'Noticias del País'
+      titulo :'Noticias del País',
+      team:[ {
+            codigo: 1,
+            nombre: 'Luis Cortés',
+            descripcion: 'Universidad Libre, Ingeniería Industrial Pasatiempo: Leer, tocar un instrumento musical guitarra.',
+            rol: 'Desarrollador Front End Y Front End',
+            image: './img/Luis.jpg'
+            },
+            {
+            codigo: 2,
+            nombre: 'Edson Suarez',
+            descripcion: 'Universidad Industrial de Santander Pasatiempo: Jugar videojuegos, tocar guitarra, practicar baloncesto, filososfar.',
+            rol: 'Desarrollador Front End',
+            image: './img/Edson.jpg'
+            },
+            {
+            codigo: 3,
+            nombre: 'Omar Arias',
+            descripcion: 'Ingeniería mecánica en la Universidad Distrital. Pasatiempo:leer, dibujar, escuchar música y practicar inglés y alemán.',
+            rol: 'Desarrollador Front End Y Front End',
+            image: './img/omar.png'
+            },
+            {
+            codigo: 4,
+            nombre: 'Adriana Pérez',
+            descripcion: 'Universidad Pedagógica y Tecnológica de Colombia Pasatiempo: Leer, Ver Series.',
+            rol: 'Desarrollador Front End',
+            image: './img/Adriana.jpg'
+            } ]
     }
   }
 }
