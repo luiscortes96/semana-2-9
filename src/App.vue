aff455aeffb5443a55f9f10315f68c1d261f5fb0
AdrianaPerezC <adrianaperez.crist@gmail.com>
2020-12-05 12:46:34 -0500

    Modificacion de equipo utilizando el componente

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,60 @@
 <template>
   <div id="app">
+  
+  <div class="row row-cols-4 row-cols-lg-4 container-fluid mx-auto py-4  ">
+  <div class="col-lg-3 col-md-6 mb-4 mb-lg-0" v-for="(item, index) of team" :key="index">
+  <TeamCard v-bind:member="item"></TeamCard>
+  
   </div>
+  </div>
+  </div>
+
 </template>
 
 <script>
 
+import TeamCard from './components/TeamCard.vue'
+
 export default {
   name: 'App',
+  components:{
+    TeamCard
+  },
+  data(){
+    return{
+      title:'Hola',
+       team:[ {
+            codigo: 1,
+            nombre: 'Luis Cortés',
+            descripcion: 'Universidad Libre, Ingeniería Industrial Pasatiempo: Leer, tocar un instrumento musical guitarra luiscortes1996@hotmail.com',
+            rol: 'Desarrollador Front End Y Front End',
+            image: './img/Luis.jpg'
+            },
+            {
+            codigo: 2,
+            nombre: 'Edson Suarez',
+            descripcion: 'Universidad Industrial de Santander Pasatiempo: Jugar videojuegos, tocar guitarra, practicar baloncesto, filososfar edsondz43@gmail.com',
+            rol: 'Desarrollador Front End',
+            image: './img/Edson.jpg'
+            },
+            {
+            codigo: 3,
+            nombre: 'Omar Arias',
+            descripcion: 'Ingeniería mecánica en la Universidad Distrital. Pasatiempo:leer, dibujar, escuchar música y practicar inglés y alemán omar_9478@hotmail.com',
+            rol: 'Desarrollador Front End Y Front End',
+            image: './img/omar.png'
+            },
+            {
+            codigo: 4,
+            nombre: 'Adriana Pérez',
+            descripcion: 'Universidad Pedagógica y Tecnológica de Colombia Pasatiempo: Leer, Ver Series adrianaperez.crist@gmail.com',
+            rol: 'Desarrollador Front End',
+            image: './img/Adriana.jpg'
+            }       
+
+  ]
+    }
+  }
 }
 </script>
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
+        
+            <!-- Card-->
 
-    </div>
+            <div class="card shadow-sm border-0 rounded" >
+                <div class="card-body p-0"><img :src="member.image" alt="Imagen del equipo"  class="w-100 card-img-top">
+                    <div class="p-4">
+                        <h5 class="mb-0">{{member.nombre}}</h5>
+                        <p class="small text-muted"> {{member.rol}}</p>
+                        <p class="text-justify">{{member.descripcion}}</p>
+                    </div>
+                </div>
+            </div>
+
+
 </template>
 
 <script>
     export default {
         name: "TeamCard",
-        props: ['member']
+        props: [ 'member'],
     }
 </script>
+
 
 <style scoped>
 
