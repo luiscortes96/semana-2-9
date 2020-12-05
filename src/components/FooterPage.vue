a0c9bce9e13b7b925ea73512cd58a49264188b29
luiscortes96 <luiscortes1996@hotmail.com>
2020-12-05 15:10:09 -0500

    cambio

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,47 @@
+ <template>
+     <div>
+        <footer class="page-footer font-small teal pt-4 container-fluid mx-auto py-4 mt-2 ">
+            <div class="container-fluid text-center text-md-left">
+                <div class="row">
+                    <div class="col-md-6 mt-md-0 mt-3">
+                         <p>Esta página web es una página diseñada para exponer nuevos cursos virtuales gratuitos
+                            acerca de temas relacionados con desarrollo web, Bases de datos, Big Data.  <br> <br>
+                            Cualquier usuario puede acceder a la educación de alta calidad y fácil acceso
+                            en la mayoría de temas relaconados con tecnología.
+                        </p>
+                        <p>
+                        <a href="https://github.com/luiscortes96/semana-2-9" class="btn btn-dark" role="button">Repositorio en GitHub</a>
+                        </p>
+                    
+  
+                        </div>
+                        <hr class="clearfix w-100 d-md-none pb-3">
+                        <div class="col-md-6 mb-md-0 mb-3">
+                        <h5 class="text-uppercase font-weight-bold">Integrantes</h5>
+                            <ul>
+                                <li> Luis Cortés, <b>Contacto:</b> luiscortes1996@hotmail.com </li>
+                                <li> Edson Suarez, <b>Contacto:</b> edsondz43@gmail.com </li>
+                                <li> Omar Arias, <b>Contacto:</b> omar_9478@hotmail.com </li>
+                                <li> Adriana Pérez, <b>Contacto:</b> adrianaperez.crist@gmail.com </li>
+                            </ul>
+                
+                        </div>
+                    </div>
+                    </div>
+    </footer>
+         
+</div>
+ </template>
+ 
+ 
+<script>
+    export default {
+        name: "FooterPage",
+
+    }
+</script>
+
+
+<style scoped>
+
+</style>
