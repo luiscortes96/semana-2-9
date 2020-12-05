f04455dbe2c72a49dee31abac0b9973cec8472a1
luiscortes96 <luiscortes1996@hotmail.com>
2020-12-05 14:41:40 -0500

    tamaños modificados

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,54 @@
 <template>
   <div id="app">
   
-  <div id="team" class="row row-cols-4 row-cols-lg-4 container-fluid mx-auto py-4  ">
-  <div class="col-lg-3 col-md-6 mb-4 mb-lg-0" v-for="(item, index) of team" :key="index">
-  <TeamCard v-bind:member="item"></TeamCard>
-  
+  <div id="team" class="row row-cols-1 row-cols-lg-6  row-cols-sm-2 container-fluid mx-auto py-4 mx-2">
+    <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 "  v-for="(item, index) of team" :key="index">
+      <TeamCard v-bind:member="item"></TeamCard>
+    </div>
   </div>
-  </div>
+  <footer-page> </footer-page>
   </div>
 
 </template>
 
 <script>
+import FooterPage from './components/FooterPage.vue'
 
 import TeamCard from './components/TeamCard.vue'
 
 export default {
   name: 'App',
   components:{
-    TeamCard
+    TeamCard,
+    FooterPage
   },
   data(){
     return{
        team:[ {
             codigo: 1,
             nombre: 'Luis Cortés',
-            descripcion: 'Universidad Libre, Ingeniería Industrial Pasatiempo: Leer, tocar guitarra.',
-            rol: 'Desarrollador Front End y Back End',
+            descripcion: 'Universidad Libre, Ingeniería Industrial',
+            rol: 'Desarrollador Front End Y Back End',
             image: './img/Luis.jpg'
             },
             {
             codigo: 2,
             nombre: 'Edson Suarez',
-            descripcion: 'Universidad Industrial de Santander Pasatiempo: Jugar videojuegos, tocar guitarra, practicar baloncesto, filosofar.',
+            descripcion: 'Universidad Industrial de Santander',
             rol: 'Desarrollador Front End',
             image: './img/Edson.jpg'
             },
             {
             codigo: 3,
             nombre: 'Omar Arias',
-            descripcion: 'Ingeniería mecánica en la Universidad Distrital. Pasatiempo:leer, dibujar, escuchar música y practicar inglés y alemán.',
+            descripcion: 'Ingeniería mecánica en la Universidad Distrital',
             rol: 'Desarrollador Front End',
             image: './img/omar.png'
             },
             {
             codigo: 4,
             nombre: 'Adriana Pérez',
-            descripcion: 'Universidad Pedagógica y Tecnológica de Colombia Pasatiempo: Leer, Ver Series.',
+            descripcion: 'Universidad Pedagógica y Tecnológica de Colombia.',
             rol: 'Desarrollador Front End y Back End',
             image: './img/Adriana.jpg'
             }       
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -2,12 +2,13 @@
         
             <!-- Card-->
 
-            <div class="card shadow-sm border-0 rounded" >
-                <div class="card-body p-0"><img :src="member.image" alt="Imagen del equipo"  class="w-100 card-img-top">
+            <div class="card shadow-sm border-0 rounded" style="height: 500px;">
+                <div class="card-body ">
+                    <img :src="member.image" alt="Imagen del equipo"  class="w-100 card-img-top h-50">
                     <div class="p-4">
                         <h5 class="mb-0">{{member.nombre}}</h5>
                         <p class="small text-muted"> {{member.rol}}</p>
-                        <p class="text-justify">{{member.descripcion}}</p>
+                        <p class="text-left">{{member.descripcion}}</p>
                     </div>
                 </div>
             </div>
